fd95ec64fb9529b6496d0839c6142492d3eb930b
Johan Botes <johan.botes@udexx.com>
2022-10-28 17:38:28 -0500

    21 st Commit - changed routes for IndexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -10,38 +10,84 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
+  });contact 
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, value) => {
-  console.log('PUT request to update the jateDB');
-  // connect to DB and version we want to use
+// Export a function we will use to POST to the database.
+export const postDb = async (name, home, cell, email)  => {
+  console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-  // make new transaction...need to specify the DB we are posting to and the data privileges. 
+
+  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
-  // open the object store
-  const objStore = tx.objectStore('jate');
-  // use the .add() method to pass in content
-  const req = objStore.put({ id: id, value: value })
-  // confirm the data was added
-  const res = await req;
-  console.log('data saved to the jateDB', res);
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+;
+
+// Export a function we will use to GET to the database.
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (value) => {
-  console.log('Getting data from the jateDB');
-  // connect to DB and version we want to use
+export const putDb = async (content) => { 
+  console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1);
+    const text = jateDb.transaction('jate', 'readwrite');
+    const store = text.objectStore('jate');
+    const request = store.put({ id: 1, content: content });
+    const result = await request;
+    console.log('Jate info data saved to the database', result);
+  };
+
+// Export a function we will use to DELETE to the database.
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+
+  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-  // make new transaction...need to specify the DB we are posting to and the data privileges. 
+
+  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
-  // open the object store
-  const objStore = tx.objectStore('jate');
-  // use the .getAll() method to grab all the content in the DB
-  const req = objStore.getAll()
-  // confirm the data was fetched
-  const res = await req;
-  console.log('data saved to the jateDB', res);
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .delete() method to get all data in the database.
+  const request = store.delete(id);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
 };
 
+// Start the database.
 initdb();
